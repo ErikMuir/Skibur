d0469f34e6677368ea64a39cbe4a47d02eb91a5f
ErikMuir <erikdmuir@gmail.com>
2016-02-18 22:38:05 -0600

    Updates to Scramble console app

--- Scramble/Program.cs
+++ Scramble/Program.cs
@@ -1,42 +1,84 @@
 ﻿using System;
+using System.Timers;
 
 namespace Scramble
 {
     class Program
     {
+        private static int _turnCount = 20;
+        private static double _ms;
+        private static Timer _timer = new Timer();
+
         static void Main(string[] args)
         {
-            var faces = new[] {"U", "D", "L", "R", "F", "B"};
-            var directions = new[] {"", "'", "2"};
-            var rnd = new Random();
-            int turnCount;
-            var scramble = "";
-            var previousFace = -1;
+            if (args.Length > 0)
+                if (!int.TryParse(args[0], out _turnCount))
+                    _turnCount = 20;
+
+            _timer.Elapsed += ShowTimer;
+            _timer.Interval = 1000;
+            _timer.Enabled = false;
 
             Console.Write(Header());
-            Console.Write("   How many turns [20] ? ");
 
-            var turnCountStr = Console.ReadLine();
-            if (!int.TryParse(turnCountStr, out turnCount))
-                turnCount = 20;
+            var isRunning = true;
 
-            scramble += "\n   ";
-            for (var i = 0; i < turnCount; i++)
+            while (isRunning)
+            {
+                var input = Console.ReadKey(true);
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.Enter:
+                        if (!_timer.Enabled) Console.Write(Scramble());
+                        break;
+                    case ConsoleKey.Spacebar:
+                        _ms = 0;
+                        _timer.Enabled = !_timer.Enabled;
+                        if (!_timer.Enabled)
+                            Console.Write("\n\n   ");
+                        else
+                            Console.Write("\r   {0}", TimeSpan.FromMilliseconds(_ms).ToString(@"mm\:ss"));
+                        break;
+                    case ConsoleKey.Escape:
+                        isRunning = false;
+                        break;
+                }
+            }
+
+            Environment.Exit(0);
+        }
+
+        private static void ShowTimer(object source, ElapsedEventArgs e)
+        {
+            _ms += _timer.Interval;
+            Console.Write("\r   {0}", TimeSpan.FromMilliseconds(_ms).ToString(@"mm\:ss"));
+        }
+
+        private static string Scramble()
+        {
+            var rnd = new Random();
+            var previousFace = -1;
+            var faces = new[] {'U', 'D', 'L', 'R', 'F', 'B'};
+            var directions = new[] { Char.MinValue, '\'', '2' };
+            var scramble = "";
+
+            for (var i = 0; i < _turnCount; i++)
             {
                 int face;
                 do
                 {
                     face = rnd.Next(faces.Length);
                 } while (face == previousFace);
-                
+
                 var direction = rnd.Next(directions.Length);
                 scramble += string.Format("{0}{1} ", faces[face], directions[direction]);
                 previousFace = face;
             }
-            scramble += "\n\n   press any key to exit...";
 
-            Console.Write(scramble);
-            Console.ReadKey();
+            scramble += "\n\n   ";
+
+            return scramble;
         }
 
         private static string Header()
@@ -120,8 +162,18 @@ namespace Scramble
                 header += horizontal;
             header += vertical;
             header += newLine;
+
+            // Instructions
             header += newLine;
             header += newLine;
+            header += "   <Enter> : scramble";
+            header += newLine;
+            header += "   <Space> : start/stop timer";
+            header += newLine;
+            header += "   <Esc>   : exit";
+            header += newLine;
+            header += newLine;
+            header += "   ";
 
             return header;
         }
